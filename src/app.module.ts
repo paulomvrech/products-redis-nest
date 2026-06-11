9a4f366ae07bc3797117fca6805463bce9f1dbb5
Paulo Vrech <paulovrech@gmail.com>
2026-06-11 14:15:18 -0300

    feat: init redis configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {CacheModule} from "@nestjs/cache-manager";
+import {createKeyv} from "@keyv/redis";
+import {PrismaModule} from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+    imports: [
+        ConfigModule.forRoot({isGlobal: true}),
+        CacheModule.registerAsync({
+            isGlobal: true,
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                const url = config.get<string>('REDIS_URL');
+                return {
+                    stores: [createKeyv(url)],
+                    ttl: 30_000, // 30 segundos, em MILISSEGUNDOS
+                }
+            }
+        }),
+        PrismaModule,
+
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
